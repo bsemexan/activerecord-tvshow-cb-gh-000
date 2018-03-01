2082921a1c313d1237c9bf9ec540354765a6a85f
Berry  Semexan <bsemexan@gmail.com>
2018-03-01 03:25:35 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,10 @@
 CreateShows < ActiveRecord::Migration[4.2]
+  def change
+    create_table :shows do |t|
+      t.string    :name
+      t.string    :network
+      t.string    :day
+      t.integer   :rating
+    end
+  end
 end
