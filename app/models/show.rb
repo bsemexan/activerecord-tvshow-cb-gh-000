2661c7d5dd75afcbf59f2e94230a7f51cdf2084b
Berry  Semexan <bsemexan@gmail.com>
2018-03-01 04:07:03 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -23,7 +23,7 @@ class Show < ActiveRecord::Base
     Show.where('rating > ?', 5)
   end
 
-  def  shows_by_alphabetical_order
+  def  self.shows_by_alphabetical_order
     Show.all.order(:name)
   end
 end
